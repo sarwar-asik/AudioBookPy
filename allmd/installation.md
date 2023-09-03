497ca321fee9ee78ec46fd6b424059ac377741eb
sarwar-asik <sarwarasik@gmail.com>
2023-09-04 01:34:17 +0600

    error in pdfReader

--- allmd/installation.md
+++ allmd/installation.md
@@ -1,3 +1,17 @@
 ### Installation and steps
 
                 pip install pyttsx3
+
+##### first time talk :::
+        
+        import pyttsx3
+        
+        friend = pyttsx3.init()
+        friend.say("I am ready to speak  Now. Thank  Boltu")
+        friend.runAndWait()
+
+import a pdf file 
+
+                pip install PyPDF2
+
+
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pyttsx3
+import PyPDF2
+book = open('oop.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+page = pdfReader.numPages
+print(page)
 friend = pyttsx3.init()
-friend.say("I am ready to speak  Now. Thank  Asik")
+page=pdfReader.getPage(7)
+text = page.extractText()
+friend.say(text)
+# friend.say("I am ready to speak  Now. Thank  Boltu")
 friend.runAndWait()
+
+
